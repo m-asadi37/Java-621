cbf4958ab45a56ca2b329cc25bc288986641c8f7
m.asadi <masd9617@gmail.com>
2025-12-09 19:23:32 +0330

    session 8

---
+++ code08/src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+
+    //property
+    String color;
+    double price;
+    int km;
+    String model;
+    int cylinders;
+    int yearModel;
+    boolean hasSanroof;
+
+    //method
+    //[return-type] [method-name] ([arg0], arg[1], ...)
+}
---
+++ code08/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i1 = 12;
+
+        Car car1 = new Car();
+        car1.km = 10;
+        car1.hasSanroof = true;
+        car1.model = "KIA";
+        car1.color = "Red";
+
+        Car car2 = new Car();
+        car2.km = 0;
+        car2.hasSanroof = false;
+        car2.model = "KIA";
+        car2.color = "BLUE";
+    }
+}
