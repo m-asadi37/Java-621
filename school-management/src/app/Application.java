a47f62253ef6bee10231af7f28bb717358fb1316
m.asadi <masd9617@gmail.com>
2026-02-17 20:08:38 +0330

    session 23

--- school-management/src/app/Application.java
+++ school-management/src/app/Application.java
@@ -1,6 +1,7 @@
 package app;
 
 import entity.Student;
+import entity.Teacher;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -9,6 +10,7 @@ import java.util.Scanner;
 public class Application {
     private static Scanner scanner = new Scanner(System.in);
     private static Student[] students = new Student[10];
+    private static Teacher[] teachers = new Teacher[10];
 
     public static void main(String[] args) {
         scanner.useDelimiter("\n");
@@ -33,10 +35,114 @@ public class Application {
                         }
                     }
                 }
+                case 2 -> {
+                    boolean isBack = false;
+                    while (!isBack) {
+                        int submenu = printTeacherMenu();
+                        switch (submenu) {
+                            case 1 -> addTeacher();
+                            case 2 -> removeTeacher();
+                            case 3 -> updateTeacher();
+                            case 4 -> infoTeacher();
+                            case 5 -> showTeacherList();
+                            case 0 -> isBack = true;
+                        }
+                    }
+                }
                 case 0 -> isExit = true;
             }
 
         }
+    }
+
+    private static void showTeacherList() {
+        for (Teacher find : teachers) {
+            if(find != null){
+                find.showInfo();
+            }
+        }
+    }
+
+    private static void infoTeacher() {
+        System.out.print("ID: ");
+        int id = scanner.nextInt();
+
+        for (Teacher find : teachers) {
+            if(find != null && find.getId() == id){
+                find.showInfo();
+                return;
+            }
+        }
+        System.out.println(">> Teacher ID Not Found!");
+    }
+
+    private static void updateTeacher() {
+        System.out.print("id for update: ");
+        int id = scanner.nextInt();
+
+        for (Teacher find : teachers) {
+            if(find != null && find.getId() == id){
+                System.out.println("new experience: ");
+                int newExperience = scanner.nextInt();
+                System.out.println("new salary: ");
+                double newSalary = scanner.nextDouble();
+
+                find.setExperience(newExperience);
+                find.setSalary(newSalary);
+
+                System.out.println(">> Teacher Experience And Salary Updated!");
+                return;
+            }
+        }
+        System.out.println(">> Teacher ID Not Found!");
+    }
+
+    private static void removeTeacher() {
+        System.out.print("id for remove: ");
+        int id = scanner.nextInt();
+
+        for (int i = 0; i < teachers.length; i++) {
+            if (teachers[i] != null && teachers[i].getId() == id) {
+                teachers[i] = null;
+                System.out.println(">> Teacher removed successfully!");
+                return;
+            }
+        }
+        System.out.println(">> Teacher Not Found!");
+    }
+
+    private static void addTeacher() {
+        System.out.print("id: ");
+        int id = scanner.nextInt();
+        System.out.print("name: ");
+        String name = scanner.next();
+        System.out.print("family: ");
+        String family = scanner.next();
+        System.out.print("salary: ");
+        double salary = scanner.nextDouble();
+        System.out.print("experience: ");
+        int experience = scanner.nextInt();
+
+        Teacher teacher = new Teacher(id, name, family, experience, salary);
+        for (int i = 0; i < teachers.length; i++) {
+            if (teachers[i] == null) {
+                teachers[i] = teacher;
+                System.out.println(">> Teacher added!");
+                break;
+            }
+        }
+    }
+
+    private static int printTeacherMenu() {
+        System.out.println("*** TEACHER MENU ***");
+        System.out.println("1. add");
+        System.out.println("2. remove");
+        System.out.println("3. update");
+        System.out.println("4. info");
+        System.out.println("5. list");
+        System.out.println("0. back");
+
+        return scanner.nextInt();
     }
 
     private static void showStudentsList() {
--- school-management/src/entity/Teacher.java
+++ school-management/src/entity/Teacher.java
@@ -1,4 +1,63 @@
 package entity;
 
 public class Teacher {
+
+    private int id;
+    private String name;
+    private String family;
+    private int experience;
+    private double salary;
+
+    public Teacher(int id, String name, String family, int experience, double salary) {
+        this.id = id;
+        this.name = name;
+        this.family = family;
+        this.experience = experience;
+        this.salary = salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void showInfo() {
+        System.out.printf("[id: '%d', name:'%s', family:'%s', experience:'%d', salary:'%,.0f']\n",
+                id, name, family, experience, salary);
+    }
 }
